dc0d548fdd1a46eb6c0dade6fcae8e53ef5fa215
mgh109 <mgh109@gmail.com>
2026-09-26 03:03:19 -0700

    Configure polling and expose workflow approvals and dashboard

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,13 @@
 import { BrandLogo } from "./components/brand-logo";
 import { WorkflowList } from "./components/workflow-list";
+import { TopMenu } from "./components/top-menu";
 
 export default function HomePage() {
   return (
     <main className="shell">
       <header className="topbar">
         <BrandLogo />
+        <TopMenu />
         <span>فضای کاری محتوا</span>
       </header>
       <WorkflowList />
